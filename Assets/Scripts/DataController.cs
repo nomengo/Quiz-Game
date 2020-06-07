5ac6965a557a03f352fbfd038d5c2c485324b16c
Levent Kırımgeri <nomengo667@gmail.com>
2020-06-07 08:45:23 -0800

    First Full Entry

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+    public RoundData[] allRoundDatas;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundDatas[0];
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Transform answerButtonParent;
+    public GameObject questionPanel;
+    public GameObject roundOverPanel;
+
+    public Text questionText;
+    public Text scoreText;
+    public Text timeRemainingText;
+
+    public SimpleObjectPool answerButtonObjectPool;
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+    private bool isRoundActive;
+    private float timeRemaining;
+    private int questionIndex;
+    private int playerScore;
+
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+    void Start()
+    {
+        dataController = FindObjectOfType<DataController>();
+        currentRoundData = dataController.GetCurrentRoundData();
+        questionPool = currentRoundData.questions;
+        timeRemaining = currentRoundData.timeLimitInSeconds;
+        UpdateTimeRemainingDisplay();
+
+        playerScore = 0;
+        questionIndex = 0;
+
+        ShowQuestions();
+        isRoundActive = true;
+    }
+
+    private void ShowQuestions()
+    {
+        RemoveRemainingButtons();
+        QuestionData questionData = questionPool[questionIndex];
+        questionText.text = questionData.questionText;
+
+        for(int i = 0;i < questionData.answers.Length; i++)
+        {
+            GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
+            answerButtonGameObject.transform.SetParent(answerButtonParent);
+            answerButtonGameObjects.Add(answerButtonGameObject);
+
+            AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+            answerButton.Setup(questionData.answers[i]);
+        }
+    }
+
+    private void RemoveRemainingButtons()
+    {
+        while (answerButtonGameObjects.Count > 0) 
+        {
+            answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+        }
+    }
+
+    public void AnswerButtonClicked(bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            playerScore += currentRoundData.pointsAddedForCorrectAnswers;
+            scoreText.text = "Score: " + playerScore.ToString();
+        }
+
+        if(questionPool.Length > questionIndex + 1)
+        {
+            questionIndex++;
+            ShowQuestions();
+        }
+        else
+        {
+            EndRound();
+        }
+    }
+
+    public void EndRound()
+    {
+        isRoundActive = false;
+
+        questionPanel.SetActive(false);
+        roundOverPanel.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    private void UpdateTimeRemainingDisplay()
+    {
+        timeRemainingText.text = "Time: " + Mathf.Round(timeRemaining).ToString();
+    }
+
+    
+    void Update()
+    {
+        if (isRoundActive)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimeRemainingDisplay();
+
+            if(timeRemaining < 0f)
+            {
+                EndRound();
+            }
+        }
+        
+    }
+}
